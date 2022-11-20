dae6eda81b62c27b2b223b441136294dcd384bc1
emilekm2142 <emilm7843@gmail.com>
2022-11-20 08:01:40 +0100

    ffrontline

--- Assets/Scripts/FrontlinePointHolder.cs
+++ Assets/Scripts/FrontlinePointHolder.cs
@@ -5,4 +5,6 @@ using UnityEngine;
         public Vector2 point;
         public List<Vector2> associatedPointsTop=null;
         public List<Vector2> associatedPointsBottom=null;
+        public List<Vector2> associatedPointsTopOffset = null;
+        public List<Vector2> associatedPointsBottomOffset = null;
     }
